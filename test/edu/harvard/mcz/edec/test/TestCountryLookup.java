cdc89bda54b7fd2a6014e33676c176ebc75e0831
Paul J. Morris <mole@morris.net>
2016-04-18 11:26:29 -0400

    ISSUE: BugID: 1630 PURPOSE: Adding unit tests of country code lookup.  DESCRIPTION: Added test of lookup method and of setting country on Species class, added test class to test suite.  Tests pass, country code lookup is behaving as expected.

---
+++ test/edu/harvard/mcz/edec/test/TestCountryLookup.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package edu.harvard.mcz.edec.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.harvard.mcz.edec.Country;
+import edu.harvard.mcz.edec.Species;
+
+/**
+ * @author mole
+ *
+ */
+public class TestCountryLookup {
+
+	/**
+	 * Test method for {@link edu.harvard.mcz.edec.Country#getCodeForCountryName(java.lang.String)}.
+	 */
+	@Test
+	public void testGetCodeForCountryName() {
+		Country country = new Country();
+		assertEquals("MX",country.getCodeForCountryName("Mexico"));
+		
+		assertEquals("",country.getCodeForCountryName("Axzzzzz"));
+	}
+	
+	@Test
+	public void testSettingCountryCode() { 
+		Species species = new Species();
+		species.setCountryOfOrigin("Mexico");
+		assertEquals("MX",species.getCountryOfOrigin());
+		species.setCountryOfOrigin("Axzzzzzz");
+		assertEquals("Ax",species.getCountryOfOrigin());
+		species.setCountryOfOrigin("AA");
+		assertEquals("AA",species.getCountryOfOrigin());
+		species.setCountryOfOrigin("bb");
+		assertEquals("bb",species.getCountryOfOrigin());		
+	}
+
+}
